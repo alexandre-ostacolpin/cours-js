8a71bcb424b5e12873d066180ebba40fff185ec6
alexandre-ostacolpin <69342542+alexandre-ostacolpin@users.noreply.github.com>
2020-11-06 16:29:43 +0100

    JS regis

---
+++ video_colombbus/js/memo/01-bases.js
@@ -0,0 +1,14 @@
+// LES BASES DE JAVASCRIPT
+
+// On parle habituellement de JavaScript ou JS.
+// Il s'agit d'un langage de script : cf. cours sur les machines
+// C'est à dire qu'on peut directement écrire le programme en JS, le programme 
+// n'aura pas besoin d'être compilé.
+
+// Histoire du JS:
+// - c'est un langage de programmation
+// - JS a été inventé en 1995 par Brendan Eich
+// - il est standardisé sous le nom d'ECMAScript
+
+// ATTENTION Bien que les 2 langages aient un nom se ressemblant, JAVA n'a 
+// ATTENTION rien à voir avec JAVASCRIPT.
