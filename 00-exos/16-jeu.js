e7b2069652c95dcb9677e7c77033a1dcf94393f4
alexandre-ostacolpin <69342542+alexandre-ostacolpin@users.noreply.github.com>
2020-12-01 16:57:25 +0100

    js

--- 00-exos/16-jeu.js
+++ 00-exos/16-jeu.js
@@ -1,6 +1,18 @@
 
-var OuiNon = oui;
+// var OuiNon = oui;
 
-while ((OuiNon = oui) || (OuiNon = non)){
-    OuiNon = prompt('Alors on joue au ni oui ni non');
+// while ((OuiNon = oui) || (OuiNon = non)){
+//     OuiNon = prompt('Alors on joue au ni oui ni non');
+// }
+
+var disMoi ='';//Initialisation de la variable avec une string vide, on y rentrera la réponse de l'utilisateur.
+console.log(typeof disMoi, disMoi);
+var i = 1;
+
+while ((disMoi !== 'oui') && (disMoi !== 'non')){
+    disMoi = prompt('On joue au ni oui ni non ?? Allez !');
+    i++;
 }
+
+document.getElementById('p1').innerHTML = ('<p> Perdu </p>')
+    
